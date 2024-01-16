31459f04a4f9491ec53c661203fcff5d1fce41e1
jeanmmlima <jeanmariomlima@gmail.com>
2024-01-16 15:24:45 -0300

    first commit spring mvc app

---
+++ spring-mvc-app/src/test/java/com/jeanlima/springmvcapp/SpringMvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanlima.springmvcapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
