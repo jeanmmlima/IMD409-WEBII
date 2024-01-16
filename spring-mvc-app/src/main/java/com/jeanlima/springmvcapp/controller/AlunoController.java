be3390d164e24673c1bfac3482086ebad5dc8253
jeanmmlima <jeanmariomlima@gmail.com>
2024-01-16 16:18:54 -0300

    exemplo alunos

---
+++ spring-mvc-app/src/main/java/com/jeanlima/springmvcapp/controller/AlunoController.java
@@ -0,0 +1,55 @@
+package com.jeanlima.springmvcapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jeanlima.springmvcapp.model.Aluno;
+import com.jeanlima.springmvcapp.service.AlunoService;
+
+@Controller
+@RequestMapping("/aluno")
+public class AlunoController {
+
+
+    @Autowired
+    @Qualifier("alunoServiceImpl")
+    AlunoService alunoService;
+
+    private final String[] cursos = {
+        "BTI", "EngSoft", "EngComp", "CienComp"
+    };
+
+    @RequestMapping("/showForm")
+    public String showFormAluno(Model model){
+
+        model.addAttribute("aluno", new Aluno());
+        model.addAttribute("cursos", cursos);
+        return "aluno/formAluno";
+    }
+
+    @RequestMapping("/addAluno")
+    public String showFormAluno(@ModelAttribute("aluno") Aluno aluno,  Model model){
+
+        alunoService.salvarAluno(aluno);
+        model.addAttribute("aluno", aluno);
+        return "aluno/paginaAluno";
+    }
+
+    @RequestMapping("/getListaAlunos")
+    public String showListaAluno(Model model){
+
+        List<Aluno> alunos = alunoService.getListaAluno();
+        model.addAttribute("alunos",alunos);
+        return "aluno/listaAlunos";
+
+    }
+
+    
+    
+}
