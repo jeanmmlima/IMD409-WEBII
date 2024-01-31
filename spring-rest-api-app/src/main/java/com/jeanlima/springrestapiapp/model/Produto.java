444fe97eda4ad378556cfc39171ce059ae74de54
jeanmmlima <jeanmariomlima@gmail.com>
2024-01-31 10:31:42 -0300

    produto controller ok

--- spring-rest-api-app/src/main/java/com/jeanlima/springrestapiapp/model/Produto.java
+++ spring-rest-api-app/src/main/java/com/jeanlima/springrestapiapp/model/Produto.java
@@ -44,6 +44,53 @@ public class Produto {
     public void setPreco(BigDecimal preco) {
         this.preco = preco;
     }
+    public Produto() {
+    }
+    public Produto(String descricao, BigDecimal preco) {
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        result = prime * result + ((preco == null) ? 0 : preco.hashCode());
+        return result;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        if (preco == null) {
+            if (other.preco != null)
+                return false;
+        } else if (!preco.equals(other.preco))
+            return false;
+        return true;
+    }
+    @Override
+    public String toString() {
+        return "Produto [id=" + id + ", descricao=" + descricao + ", preco=" + preco + "]";
+    }
+
+    
 
     
     
