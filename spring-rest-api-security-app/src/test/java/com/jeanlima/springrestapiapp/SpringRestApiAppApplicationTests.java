7d8132b0c2829e29e39d7858fcf46cf38681a178
jeanmmlima <jeanmariomlima@gmail.com>
2024-02-07 15:04:34 -0300

    arquivos iniciais para spring security

---
+++ spring-rest-api-security-app/src/test/java/com/jeanlima/springrestapiapp/SpringRestApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanlima.springrestapiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
