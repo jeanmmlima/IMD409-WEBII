cf2dd0c6d1c2b30c3e3982f8ed927b0e5339e34d
jeanmmlima <jeanmariomlima@gmail.com>
2024-05-06 16:09:18 -0300

    eureka-server-init

---
+++ spring-microservices-app/eureka-server/src/test/java/com/jeanlima/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanlima.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
