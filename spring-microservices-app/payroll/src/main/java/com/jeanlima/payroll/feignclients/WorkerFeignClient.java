2f9e91c8c7d9b6752e5d35f085b3784698ab3dd9
jeanmmlima <jeanmariomlima@gmail.com>
2024-05-06 15:45:39 -0300

    usando feign client para requisicoes

---
+++ spring-microservices-app/payroll/src/main/java/com/jeanlima/payroll/feignclients/WorkerFeignClient.java
@@ -0,0 +1,17 @@
+package com.jeanlima.payroll.feignclients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.jeanlima.payroll.model.Worker;
+
+@Component
+@FeignClient(name = "worker",url = "localhost:8001",path = "/workers")
+public interface WorkerFeignClient {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Worker> findById(@PathVariable Long id);
+}
--- spring-microservices-app/payroll/src/main/java/com/jeanlima/payroll/services/PaymentService.java
+++ spring-microservices-app/payroll/src/main/java/com/jeanlima/payroll/services/PaymentService.java
@@ -1,33 +1,23 @@
 package com.jeanlima.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
+import com.jeanlima.payroll.feignclients.WorkerFeignClient;
 import com.jeanlima.payroll.model.Payment;
 import com.jeanlima.payroll.model.Worker;
 
 @Service
 public class PaymentService {
 
-    @Value("${worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
-    public Payment getPayment(long workerId, int days){
+    public Payment getPayment(long workerId, int days) {
 
-        Map<String,String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
+        
 
-        Worker worker = restTemplate.getForObject(
-            workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 
