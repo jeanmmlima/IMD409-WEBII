6861490cdf354d07eb4e4ce2640bcaecd9c1087c
jeanmmlima <jeanmariomlima@gmail.com>
2024-05-08 15:29:39 -0300

    worker cliente do servidor de configuracao

--- spring-microservices-app/worker/src/main/java/com/jeanlima/worker/resources/WorkerResources.java
+++ spring-microservices-app/worker/src/main/java/com/jeanlima/worker/resources/WorkerResources.java
@@ -6,6 +6,7 @@ import org.springframework.core.env.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,9 @@ public class WorkerResources {
 
     @Autowired
     private WorkerRepositotry repository;    
+
+    @Value("${test.config}")
+    String testConfig;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
@@ -58,4 +62,11 @@ public class WorkerResources {
         Worker worker = repository.findById(id).get();
         return ResponseEntity.ok(worker);
     }
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs(){
+        logger.info("CONFIG= "+testConfig);
+        return ResponseEntity.noContent().build();
+    }
+
 }
