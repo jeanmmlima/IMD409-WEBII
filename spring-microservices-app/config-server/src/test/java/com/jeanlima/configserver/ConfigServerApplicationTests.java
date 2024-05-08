8c954b9a95c559d559ff59ac7fa5651f3013b17e
jeanmmlima <jeanmariomlima@gmail.com>
2024-05-08 15:07:37 -0300

    first commit com o servidor de configuracoes

---
+++ spring-microservices-app/config-server/src/test/java/com/jeanlima/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanlima.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
