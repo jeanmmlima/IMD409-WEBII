e8019d43d0ea04e55bdee8eacec980603c764a51
jeanmmlima <jeanmariomlima@gmail.com>
2024-05-08 15:45:18 -0300

    actuator para atualizar informacoes em tempo real

--- spring-microservices-app/worker/src/main/java/com/jeanlima/worker/resources/WorkerResources.java
+++ spring-microservices-app/worker/src/main/java/com/jeanlima/worker/resources/WorkerResources.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jeanlima.worker.entities.Worker;
 import com.jeanlima.worker.repository.WorkerRepositotry;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResources {
