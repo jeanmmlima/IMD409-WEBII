08ddf54d42f844948fa1f7a4de29609a73597afc
jeanmmlima <jeanmariomlima@gmail.com>
2024-05-24 15:51:30 -0300

    consumindo recursos do zuul com autorizacao

---
+++ spring-microservices-app/apigatewayzuul/src/main/java/com/jeanlima/apigatewayzuul/config/AppConfig.java
@@ -0,0 +1,24 @@
+package com.jeanlima.apigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter(){
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        //chave de asssinatura do token
+        tokenConverter.setSigningKey("MY-SECRETE-KEY");
+        return tokenConverter;
+    }
+
+    @Bean
+    public JwtTokenStore tokenStore(){
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+}
---
+++ spring-microservices-app/apigatewayzuul/src/main/java/com/jeanlima/apigatewayzuul/config/ResourceServerConfig.java
@@ -0,0 +1,40 @@
+package com.jeanlima.apigatewayzuul.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    private static final String[] PUBLIC = {"/oauth/oauth/token"};
+    private static final String[] OPERATOR = {"/worker/**"};
+    private static final String[] ADMIN = {"/payroll/**", "/user/**"};
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests(requests -> requests
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, OPERATOR).hasAnyRole("OPERATOR", "ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated());
+    }
+
+    
+
+}
